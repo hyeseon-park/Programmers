c533c65bf5649a2739920997d08b107cb784d245
hyeseon-park <lightning350@naver.com>
2022-07-28 17:43:19 +0900

    프린터

--- level2/printer.js
+++ level2/printer.js
@@ -1,8 +1,28 @@
-const priorities = [2, 1, 3, 2];
-const location = 2;
+const priorities = [1, 1, 9, 1, 1, 1];
+const location = 0;
 
 const solution = () => {
   let cnt = 0;
+
+  let priCopy = new Array(priorities.length).fill(0);
+  for (var i = 0; i < priorities.length; i++) {
+    priCopy[i] = priorities[i];
+  }
+  priCopy.sort((a, b) => b - a);
+
+  while (priCopy.length !== 0) {
+    for (var i = 0; i < priorities.length; i++) {
+      if (priorities[i] === priCopy[0]) {
+        priCopy.shift();
+        cnt++;
+        if (i === location) {
+          priCopy = [];
+          break;
+        }
+      }
+    }
+  }
+
   return cnt;
 };
 
