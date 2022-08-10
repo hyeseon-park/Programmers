3630c0edf2b2c61168377cc5b47d7a40008f4f19
hyeseon-park <lightning350@naver.com>
2022-08-10 18:03:45 +0900

    가장 큰 수

--- level2/maxNum.js
+++ level2/maxNum.js
@@ -1,6 +1,14 @@
-const numbers = [6, 10, 2];
+const numbers = [3, 30, 34, 5, 9];
 
 const solution = () => {
+  let ans = "";
+  numbers.sort(
+    (a, b) => Number(String(b) + String(a)) - Number(String(a) + String(b))
+  );
+  for (var i = 0; i < numbers.length; i++) {
+    if (ans === "0") ans = "";
+    ans += numbers[i];
+  }
   return ans;
 };
 
