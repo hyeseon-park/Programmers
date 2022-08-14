9debe72e1563737db3d3d2e148af8533e17fb465
hyeseon-park <lightning350@naver.com>
2022-08-14 17:11:16 +0900

    242. Valid Anagram 다른 방법

--- leetcode/ValidAnagram.js
+++ leetcode/ValidAnagram.js
@@ -10,4 +10,34 @@ var isAnagram = function (s, t) {
   return t.length === 0 ? true : false;
 };
 
+var isAnagram = function (s, t) {
+  let hashmap = new Map();
+  for (var i = 0; i < s.length; i++) {
+    let char = s.charAt(i);
+    if (hashmap.has(char)) {
+      hashmap.set(char, hashmap.get(char) + 1);
+    } else {
+      hashmap.set(char, 1);
+    }
+  }
+  for (var i = 0; i < t.length; i++) {
+    let char = t.charAt(i);
+    if (hashmap.has(char)) {
+      if (hashmap.get(char) < 1) {
+        return false;
+      } else {
+        hashmap.set(char, hashmap.get(char) - 1);
+      }
+    } else {
+      return false;
+    }
+  }
+
+  let is = true;
+  hashmap.forEach((value) => {
+    if (Number(value) !== 0) is = false;
+  });
+  return is;
+};
+
 console.log(isAnagram(s, t));
