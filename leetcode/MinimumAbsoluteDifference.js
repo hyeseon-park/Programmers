c1a21f411c51004b8c72881f043c98b1abe96e4a
hyeseon-park <lightning350@naver.com>
2022-08-14 15:28:22 +0900

    1200. Minimum Absolute Difference 다른 방법

--- leetcode/MinimumAbsoluteDifference.js
+++ leetcode/MinimumAbsoluteDifference.js
@@ -1,4 +1,4 @@
-let arr = [1, 3, 6, 10, 15];
+let arr = [3, 8, -10, 23, 19, -4, -14, 27];
 
 var minimumAbsDifference = function (arr) {
   arr.sort((a, b) => a - b);
@@ -16,6 +16,23 @@ var minimumAbsDifference = function (arr) {
     }
   }
   return [...hashmap.entries()].reduce((a, b) => (a[0] < b[0] ? a : b))[1];
+};
+
+var minimumAbsDifference = function (arr) {
+  arr.sort((a, b) => a - b);
+  let anx = [];
+  let gap = 2000000;
+  for (var i = 0; i < arr.length - 1; i++) {
+    let newGap = arr[i + 1] - arr[i];
+    if (newGap < gap) {
+      anx = [];
+      anx.push([arr[i], arr[i + 1]]);
+      gap = newGap;
+    } else if (newGap === gap) {
+      anx.push([arr[i], arr[i + 1]]);
+    }
+  }
+  return anx;
 };
 
 console.log(minimumAbsDifference(arr));
