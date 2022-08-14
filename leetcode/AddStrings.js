7187646abae7366ff7ea7b710f7029e0e9cf1e9d
hyeseon-park <lightning350@naver.com>
2022-08-14 17:24:29 +0900

    415. Add Strings 미해결

---
+++ leetcode/AddStrings.js
@@ -0,0 +1,12 @@
+let num1 = "9333852702227987";
+let num2 = "85731737104263";
+
+var addStrings = function (num1, num2) {
+  let i = num1.length - 1;
+  let j = num2.length - 1;
+  let carry = 0;
+  let sum = "";
+  for (; i >= 0 || j >= 0 || carry > 0; i--, j--) {}
+};
+
+console.log(addStrings(num1, num2));
