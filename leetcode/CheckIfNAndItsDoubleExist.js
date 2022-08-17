b4e26a46b1cb137fb40c858fed02d38deb7afe55
hyeseon-park <lightning350@naver.com>
2022-08-17 10:36:18 +0900

    1346. Check If N and Its Double Exist 다른 방법

--- leetcode/CheckIfNAndItsDoubleExist.js
+++ leetcode/CheckIfNAndItsDoubleExist.js
@@ -1,4 +1,4 @@
-let arr = [0, 0];
+let arr = [0, 0, 2, 5, 6, 3, 6, 3];
 
 var checkIfExist = function (arr) {
   for (var i = 0; i < arr.length; i++) {
@@ -9,6 +9,17 @@ var checkIfExist = function (arr) {
         return true;
       }
     }
+  }
+  return false;
+};
+
+var checkIfExist = function (arr) {
+  let set = new Set();
+  for (var i in arr) {
+    if (set.has(arr[i] * 2) || (arr[i] % 2 === 0 && set.has(arr[i] / 2))) {
+      return true;
+    }
+    set.add(arr[i]);
   }
   return false;
 };
