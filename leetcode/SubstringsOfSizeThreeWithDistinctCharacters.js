ecb73a64ec616c3a73b06e2e44398777bb688d50
hyeseon-park <lightning350@naver.com>
2022-08-19 13:22:00 +0900

    1876. Substrings of Size Three with Distinct Characters

---
+++ leetcode/SubstringsOfSizeThreeWithDistinctCharacters.js
@@ -0,0 +1,21 @@
+let s = "aababcabc";
+
+var countGoodSubstrings = function (s) {
+  let cnt = 0;
+  for (var i = 0; i < s.length - 2; i++) {
+    let tmpS = s.substring(i, i + 3);
+    let hashmap = new Map();
+    let is = true;
+    for (var j = 0; j < 3; j++) {
+      if (hashmap.has(tmpS.charAt(j))) {
+        is = false;
+        break;
+      }
+      hashmap.set(tmpS.charAt(j), 1);
+    }
+    if (is) cnt++;
+  }
+  return cnt;
+};
+
+console.log(countGoodSubstrings(s));
