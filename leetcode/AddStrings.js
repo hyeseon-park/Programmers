5df9155ce4368035b7e45197db40fa0bb2da2faa
hyeseon-park <lightning350@naver.com>
2022-08-19 09:55:48 +0900

    415. Add Strings

--- leetcode/AddStrings.js
+++ leetcode/AddStrings.js
@@ -6,7 +6,14 @@ var addStrings = function (num1, num2) {
   let j = num2.length - 1;
   let carry = 0;
   let sum = "";
-  for (; i >= 0 || j >= 0 || carry > 0; i--, j--) {}
+  for (; i >= 0 || j >= 0 || carry > 0; i--, j--) {
+    const digit1 = i < 0 ? 0 : num1.charAt(i) - "0";
+    const digit2 = j < 0 ? 0 : num2.charAt(j) - "0";
+    const digitsSum = digit1 + digit2 + carry;
+    sum = `${digitsSum % 10}${sum}`;
+    carry = Math.floor(digitsSum / 10);
+  }
+  return sum;
 };
 
 console.log(addStrings(num1, num2));
