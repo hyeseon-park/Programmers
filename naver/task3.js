a6b39e93ee2be7d8a136e60c1b9d874687970c4e
hyeseon-park <lightning350@naver.com>
2022-08-31 18:04:55 +0900

    tast3

---
+++ naver/task3.js
@@ -0,0 +1,13 @@
+let A = ["co", "dil", "ity"];
+// let A = ["abc", "yyy", "def", "csv"];
+// let A = ["patato", "kayak", "banana", "racecar"];
+// let A = ["eva", "jqw", "tyn", "jan"];
+
+const solution = (A) => {
+  let len = 0;
+
+  for (var i = 0; i < A.length; i++) {}
+  return len;
+};
+
+console.log(solution(A));
