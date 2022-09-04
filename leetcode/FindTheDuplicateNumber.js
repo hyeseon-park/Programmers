fd86684ca461cbbbd4e024c2c521cdc070c32bbc
hyeseon-park <lightning350@naver.com>
2022-09-04 15:15:29 +0900

    287. Find the Duplicate Number

---
+++ leetcode/FindTheDuplicateNumber.js
@@ -0,0 +1,13 @@
+let nums = [3, 1, 3, 4, 2];
+
+var findDuplicate = function (nums) {
+  nums.sort((a, b) => a - b);
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      return nums[i];
+    }
+  }
+  return 0;
+};
+
+console.log(findDuplicate(nums));
