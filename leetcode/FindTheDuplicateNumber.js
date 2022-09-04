18371c5ae56c9665bfa0011c184bf4bfb9d1cec5
hyeseon-park <lightning350@naver.com>
2022-09-04 15:28:59 +0900

    287. Find the Duplicate Number 다른 방법

--- leetcode/FindTheDuplicateNumber.js
+++ leetcode/FindTheDuplicateNumber.js
@@ -10,4 +10,13 @@ var findDuplicate = function (nums) {
   return 0;
 };
 
+var findDuplicate = function (nums) {
+  let arr = new Array(nums.length - 1).fill(0);
+  for (var i = 0; i < nums.length; i++) {
+    arr[nums[i] - 1]++;
+    if (arr[nums[i] - 1] > 1) return nums[i];
+  }
+  return 0;
+};
+
 console.log(findDuplicate(nums));
