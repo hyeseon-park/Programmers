a20803d9e9eecc872f59676de3c4add29614f280
hyeseon-park <lightning350@naver.com>
2022-09-05 20:48:34 +0900

    75. Sort Colors

--- leetcode/SortColors.js
+++ leetcode/SortColors.js
@@ -1,19 +1,16 @@
 let nums = [2, 0, 2, 1, 1, 0];
 
 var sortColors = function (nums) {
-  let i = 0;
-  let j = 0;
-  j++;
-  while (i < nums.length || j < nums.length) {
-    if (nums[i] > nums[i + 1]) {
-      let tmp = nums[i];
-      nums[i] = nums[i + 1];
-      nums[i + 1] = tmp;
-    } else {
+  for (var i = 0; i < nums.length; i++) {
+    for (var j = i + 1; j < nums.length; j++) {
+      if (nums[i] > nums[j]) {
+        let tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+      }
     }
-    i++;
-    j++;
   }
+
   return nums;
 };
 
