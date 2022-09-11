54079f5e87a2e2322ccecb2a97f985acc9352646
hyeseon-park <lightning350@naver.com>
2022-09-11 15:00:50 +0900

    1929. Concatenation of Array

--- leetcode/ConcatenationOfArray.js
+++ leetcode/ConcatenationOfArray.js
@@ -1,7 +1,12 @@
-let nums = [1, 2, 1];
+let nums = [1, 3, 2, 1];
 
 var getConcatenation = function (nums) {
-  return nums;
+  let ans = new Array(nums.length * 2).fill(0);
+  for (var i = 0; i < nums.length; i++) {
+    ans[i] = nums[i];
+    ans[i + nums.length] = nums[i];
+  }
+  return ans;
 };
 
 console.log(getConcatenation(nums));
