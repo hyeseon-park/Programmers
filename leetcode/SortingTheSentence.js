6898f24d0875182efdf627ee6d56707ab44946a4
hyeseon-park <lightning350@naver.com>
2022-09-11 15:31:50 +0900

    1859. Sorting the Sentence

--- leetcode/SortingTheSentence.js
+++ leetcode/SortingTheSentence.js
@@ -1,3 +1,17 @@
-var sortSentence = function (s) {};
+let s = "Myself2 Me1 I4 and3";
+
+var sortSentence = function (s) {
+  let sArr = s.split(" ");
+  let ansArr = new Array(sArr.length - 1);
+
+  for (var i = 0; i < sArr.length; i++) {
+    let word = sArr[i];
+    let tmpWord = word.substring(0, word.length - 1);
+    let tmpNum = word.substring(word.length - 1, word.length);
+    ansArr[tmpNum - 1] = tmpWord;
+  }
+
+  return ansArr.join(" ");
+};
 
 console.log(sortSentence(s));
