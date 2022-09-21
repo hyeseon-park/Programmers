4aa4b8705c7d334af50a7c19915893a400fc468f
hyeseon-park <lightning350@naver.com>
2022-09-21 17:59:35 +0900

    2391. Minimum Amount of Time to Collect Garbage

---
+++ leetcode/MinimumAmountOfTimeToCollectGarbage.js
@@ -0,0 +1,8 @@
+let garbage = ["G", "P", "GP", "GG"];
+let travel = [2, 4, 3];
+
+var garbageCollection = function (garbage, travel) {
+  return "";
+};
+
+console.log(garbageCollection(garbage, travel));
