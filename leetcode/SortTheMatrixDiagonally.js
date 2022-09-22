0f492b364dad41da80c8ec40afca2c49f1e76e01
hyeseon-park <lightning350@naver.com>
2022-09-22 17:02:27 +0900

    1329. Sort the Matrix Diagonally

--- leetcode/SortTheMatrixDiagonally.js
+++ leetcode/SortTheMatrixDiagonally.js
@@ -1,8 +1,3 @@
-// let mat = [
-//   [3, 3, 1, 1],
-//   [2, 2, 1, 2],
-//   [1, 1, 1, 2],
-// ];
 let mat = [
   [11, 25, 66, 1, 69, 7],
   [23, 55, 17, 45, 15, 52],
@@ -12,26 +7,25 @@ let mat = [
 ];
 
 var diagonalSort = function (mat) {
-  let arr = Array.from(
+  let anxArr = Array.from(Array(mat.length), () => new Array(mat[0].length));
+  let tmpArr = Array.from(
     Array(mat.length + mat[0].length - 1),
     () => new Array()
   );
 
   for (var i = 0; i < mat.length; i++) {
     for (var j = 0; j < mat[0].length; j++) {
-      arr[i - j + mat[0].length - 1].push(mat[i][j]);
+      tmpArr[i - j + mat[0].length - 1].push(mat[i][j]);
     }
   }
 
-  for (var i = 0; i < arr.length; i++) {
-    arr[i].sort((a, b) => a - b);
+  for (var i = 0; i < tmpArr.length; i++) {
+    tmpArr[i].sort((a, b) => a - b);
   }
-
-  let anxArr = Array.from(Array(mat.length), () => new Array(mat[0].length));
 
   for (var i = 0; i < anxArr.length; i++) {
     for (var j = 0; j < anxArr[0].length; j++) {
-      anxArr[i][j] = arr[i - j + mat[0].length - 1].shift();
+      anxArr[i][j] = tmpArr[i - j + mat[0].length - 1].shift();
     }
   }
 
