30e69e80389b43742f4e202811331c21a7c5fe90
hyeseon-park <lightning350@naver.com>
2022-09-22 16:52:51 +0900

    2391. Minimum Amount of Time to Collect Garbage 다른 방법

--- leetcode/MinimumAmountOfTimeToCollectGarbage.js
+++ leetcode/MinimumAmountOfTimeToCollectGarbage.js
@@ -3,27 +3,21 @@ let travel = [3, 10];
 
 var garbageCollection = function (garbage, travel) {
   let minute = 0;
-  let mDone = false;
-  let pDone = false;
-  let gDone = false;
 
+  garbage.map((c) => (minute += c.length));
+  ["M", "P", "G"].map((c) => (minute += collectGarbage(garbage, travel, c)));
+
+  return minute;
+};
+
+var collectGarbage = function (garbage, travel, type) {
+  let minute = 0;
   for (var i = garbage.length - 1; i >= 0; i--) {
-    minute += garbage[i].length;
-
-    if (garbage[i].includes("M") && mDone === false) {
-      mDone = true;
+    if (garbage[i].includes(type)) {
       for (var j = 0; j < i; j++) minute += travel[j];
-    }
-    if (garbage[i].includes("P") && pDone === false) {
-      pDone = true;
-      for (var j = 0; j < i; j++) minute += travel[j];
-    }
-    if (garbage[i].includes("G") && gDone === false) {
-      gDone = true;
-      for (var j = 0; j < i; j++) minute += travel[j];
+      break;
     }
   }
-
   return minute;
 };
 
