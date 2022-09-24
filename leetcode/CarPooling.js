795feec85a118cebff28507a05d3ca7b7c105466
hyeseon-park <lightning350@naver.com>
2022-09-24 18:19:34 +0900

    1094. Car Pooling 다른 방법

--- leetcode/CarPooling.js
+++ leetcode/CarPooling.js
@@ -10,13 +10,25 @@ var carPooling = function (trips, capacity) {
     let num = trips[i][0];
     let from = trips[i][1];
     let to = trips[i][2];
+    if (num > capacity) return false;
     for (var j = from; j < to; j++) {
-      if (num > capacity || hashmap.get(j) + num > capacity) return false;
+      if (hashmap.get(j) + num > capacity) return false;
       hashmap.set(j, hashmap.has(j) ? hashmap.get(j) + num : num);
     }
   }
 
   return true;
+};
+
+var carPooling = function (trips, capacity) {
+  let arr = new Array(1001).fill(0);
+  trips.map((t) => {
+    arr[t[1]] += t[0];
+    arr[t[2]] -= t[0];
+  });
+  for (var i = 0; i < 1001 && capacity >= 0; ++i) capacity -= arr[i];
+
+  return capacity >= 0;
 };
 
 console.log(carPooling(trips, capacity));
