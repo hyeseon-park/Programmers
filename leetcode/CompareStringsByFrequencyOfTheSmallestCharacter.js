fbb3e496ea049aad41f104d037c46fa08cfafbf7
hyeseon-park <lightning350@naver.com>
2022-10-13 11:06:32 +0900

    1170. Compare Strings by Frequency of the Smallest Character

--- leetcode/CompareStringsByFrequencyOfTheSmallestCharacter.js
+++ leetcode/CompareStringsByFrequencyOfTheSmallestCharacter.js
@@ -9,18 +9,16 @@ var numSmallerByFrequency = function (queries, words) {
   for (var i = 0; i < queries.length; i++) {
     let qNum = f(queries[i]);
     let cnt = 0;
-    for (var j = 0; j < words.length; j++) {
-      if (qNum < words[j]) cnt++;
-    }
+    for (var j = 0; j < words.length; j++) if (qNum < words[j]) cnt++;
     anxArr.push(cnt);
   }
+
   return anxArr;
 };
 
 var f = function (s) {
   let arr = s.split("").sort();
-  let result = arr.reduce((cnt, data) => (data === arr[0] ? cnt + 1 : cnt), 0);
-  return result;
+  return arr.reduce((cnt, data) => (data === arr[0] ? cnt + 1 : cnt), 0);
 };
 
 console.log(numSmallerByFrequency(queries, words));
