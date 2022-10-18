5b1fab8e7399fbf51d6a1f627cedf9188a570895
hyeseon-park <lightning350@naver.com>
2022-10-18 15:07:28 +0900

    1324. Print Words Vertically

--- leetcode/PrintWordsVertically.js
+++ leetcode/PrintWordsVertically.js
@@ -1,14 +1,30 @@
-let s = "TO BE OR NOT TO BE";
+let s = "CONTEST IS COMING";
 
 var printVertically = function (s) {
+  let arr = [];
+
   let sArr = s.split(" ");
+  let lengthArr = sArr.map((s) => s.length);
+  let maxNum = Math.max(...lengthArr);
+
   let hashmap = new Map();
   for (var i = 0; i < sArr.length; i++) {
-    for (var j = 0; j < sArr[i].length; j++) {
-      hashmap.set(j, hashmap.has(j) ? hashmap.get(j) + sArr[i][j] : sArr[i][j]);
+    for (var j = 0; j < maxNum; j++) {
+      hashmap.set(
+        j,
+        hashmap.has(j)
+          ? sArr[i][j] !== undefined
+            ? hashmap.get(j) + sArr[i][j]
+            : hashmap.get(j) + " "
+          : sArr[i][j] !== undefined
+          ? sArr[i][j]
+          : " "
+      );
     }
   }
-  return hashmap;
+  hashmap.forEach((values) => arr.push(values.trimEnd()));
+
+  return arr;
 };
 
 console.log(printVertically(s));
