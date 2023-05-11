a4bb6ad347af3e54de16b4c4dfb3961a80de1036
hyeseon-park <lightning350@naver.com>
2023-05-11 15:29:23 +0900

    가위 바위 보

---
+++ lesson/120839.js
@@ -0,0 +1,11 @@
+function solution(rsp) {
+  let arr = {
+    2: 0,
+    0: 5,
+    5: 2,
+  };
+  var answer = [...rsp].map((v) => arr[v]).join("");
+  return answer;
+}
+
+console.log(solution("205"));
