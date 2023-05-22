e19fa1e92e495a0e8b80ddf2ed233127504cf759
hyeseon-park <lightning350@naver.com>
2023-05-22 14:50:13 +0900

    깊은 복사 얕은 복사

---
+++ study/copy.js
@@ -0,0 +1,15 @@
+const a = "a";
+let b = a;
+b = "c";
+
+console.log(a); // a
+console.log(b); // c
+
+// const a = {
+//   one: 1,
+// };
+// let b = a;
+// b.one = 3;
+
+// console.log(a); // { one: 3 }
+// console.log(b); // { one: 3 }
