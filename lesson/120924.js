0e5acc1cf2c2a43c9f1d1bc3e0ff21a4ffa15ed7
hyeseon-park <lightning350@naver.com>
2023-05-27 15:55:24 +0900

    다음에 올 숫자

---
+++ lesson/120924.js
@@ -0,0 +1,9 @@
+function solution(common) {
+  if (common[1] - common[0] === common[2] - common[1]) {
+    return common[common.length - 1] + (common[1] - common[0]);
+  } else {
+    return common[common.length - 1] * (common[1] / common[0]);
+  }
+}
+
+console.log(solution([2, 4, 8]));
