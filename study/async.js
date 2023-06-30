3d0c6b312c108363203bab03289e16f578dd2cf1
hyeseon-park <lightning350@naver.com>
2023-07-01 01:06:32 +0900

    async 함수 예제

--- study/async.js
+++ study/async.js
@@ -12,3 +12,10 @@ const baz = async (n) => {
   return n;
 };
 baz(3).then((v) => console.log(v));
+
+const obj = {
+  async foo(n) {
+    return n;
+  },
+};
+obj.foo(4).then((v) => console.log(v));
