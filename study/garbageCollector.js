f7f2cbb5b0e93dd2ac258d13bc1cadc7185fb889
hyeseon-park <lightning350@naver.com>
2023-07-01 01:04:24 +0900

    가비지 콜렉터

---
+++ study/garbageCollector.js
@@ -0,0 +1,16 @@
+var score;
+score = 80;
+score = 90;
+
+console.log(score); // 90
+
+var rocket = "Of course,\rI still love you";
+
+// var rocket = `Of course,
+// I still love you`;
+
+console.log(rocket);
+
+var verb = "love";
+console.log("Of course, I still " + verb + " you");
+console.log(`Of course, I still ${verb} you`);
